7e942f82b547fe41ca2446a556082b857cdbe488
beatriz.developer <beatrizddemetria@gmail.com>
2024-11-17 21:28:23 -0400

    first commit

---
+++ src/Banco.java
@@ -0,0 +1,26 @@
+package br.com.banco.service;
+
+import br.com.banco.model.Conta;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Banco {
+    private List<Conta> contas;
+
+    public Banco() {
+        contas = new ArrayList<>();
+    }
+
+    public void adicionarConta(Conta conta) {
+        contas.add(conta);
+    }
+
+    public Conta buscarContaPorNumero(String numero) {
+        for (Conta conta : contas) {
+            if (conta.getNumero().equals(numero)) {
+                return conta;
+            }
+        }
+        return null;
+    }
+}
